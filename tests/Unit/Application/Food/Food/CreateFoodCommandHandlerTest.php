dbcf227dff5c7ded16d6c593a7c33be0fe444161
Lauric-h <h.lauric@outlook.com>
2025-02-25 13:54:35 +0100

    Add test handler

---
+++ tests/Unit/Application/Food/Food/CreateFoodCommandHandlerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Unit\Application\Food\Food;
+
+use App\Application\Food\CreateFood\CreateFoodCommand;
+use App\Application\Food\CreateFood\CreateFoodCommandHandler;
+use App\Domain\Food\Entity\Brand;
+use App\Domain\Food\Repository\BrandsCatalog;
+use PHPUnit\Framework\TestCase;
+
+final class CreateFoodCommandHandlerTest extends TestCase
+{
+    public function testCreateFood(): void
+    {
+        $command = new CreateFoodCommand(
+            id: 1,
+            brandId: 1,
+            name: 'Food name',
+            carbs: 10,
+            ingestionType: 'solid',
+            calories: 100
+        );
+
+        $repository = $this->createMock(BrandsCatalog::class);
+        $handler = new CreateFoodCommandHandler($repository);
+
+        $repository->expects($this->once())
+            ->method('get')
+            ->with($command->brandId)
+            ->willReturn(new Brand(1, 'Brand name'))
+        ;
+
+        $repository->expects($this->once())
+            ->method('add');
+
+        ($handler)($command);
+    }
+}
