d4c7a0e1074cef6a92c36df3fc5e672e7adeaeb9
Lauric-h <h.lauric@outlook.com>
2025-03-27 12:44:02 +0100

    Add UseCase CommandHandler

---
+++ src/Application/NutritionPlan/UseCase/CreateNutritionPlanCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Application\NutritionPlan\UseCase;
+
+use App\Infrastructure\Shared\Bus\CommandInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final readonly class CreateNutritionPlanCommand implements CommandInterface
+{
+    public function __construct(
+        #[Assert\Uuid]
+        public string $id,
+        #[Assert\Uuid]
+        public string $raceId,
+        #[Assert\Uuid]
+        public string $runnerId,
+    ) {
+    }
+}
---
+++ src/Application/NutritionPlan/UseCase/CreateNutritionPlanCommandHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Application\NutritionPlan\UseCase;
+
+use App\Domain\NutritionPlan\Entity\NutritionPlan;
+use App\Domain\NutritionPlan\Repository\NutritionPlansCatalog;
+use App\Infrastructure\Shared\Bus\CommandHandlerInterface;
+
+final readonly class CreateNutritionPlanCommandHandler implements CommandHandlerInterface
+{
+    public function __construct(private NutritionPlansCatalog $nutritionPlansCatalog)
+    {
+    }
+
+    public function __invoke(CreateNutritionPlanCommand $command): void
+    {
+        $nutritionPlan = new NutritionPlan(
+            $command->id,
+            $command->raceId,
+            $command->runnerId,
+        );
+
+        $this->nutritionPlansCatalog->add($nutritionPlan);
+    }
+}
