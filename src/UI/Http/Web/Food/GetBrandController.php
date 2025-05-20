25b347dd19cee88477a4646426c053647c5d6567
Lauric-h <h.lauric@outlook.com>
2025-05-20 21:32:44 +0200

    Add get brand controller

---
+++ src/UI/Http/Web/Food/GetBrandController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\UI\Http\Web\Food;
+
+use App\Application\Food\UseCase\GetBrand\GetBrandQuery;
+use App\Infrastructure\Shared\Bus\QueryBus;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/brands/{id}', name: 'app.brand.get', methods: ['GET'])]
+final class GetBrandController extends AbstractController
+{
+    public function __construct(private readonly QueryBus $queryBus)
+    {
+    }
+
+    public function __invoke(string $id): Response
+    {
+        return $this->render('Food/get_brand.html.twig', [
+            'brand' => $this->queryBus->query(new GetBrandQuery($id)),
+        ]);
+    }
+}
