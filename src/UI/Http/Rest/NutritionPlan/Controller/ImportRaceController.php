7ec66fec8735c72c05cab78b01ceed82ab58421b
Lauric-h <h.lauric@outlook.com>
2026-02-04 15:42:20 +0100

    Import race with user

--- src/UI/Http/Rest/NutritionPlan/Controller/ImportRaceController.php
+++ src/UI/Http/Rest/NutritionPlan/Controller/ImportRaceController.php
@@ -5,11 +5,13 @@ namespace App\UI\Http\Rest\NutritionPlan\Controller;
 use App\Application\NutritionPlan\UseCase\ImportRace\ImportRaceCommand;
 use App\Application\Shared\IdGeneratorInterface;
 use App\Infrastructure\Shared\Bus\CommandBus;
+use App\Infrastructure\User\Security\UserAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route('/events/{eventId}/races/{raceId}', name: 'api.event.import_race', methods: ['POST'])]
 final class ImportRaceController extends AbstractController
@@ -21,14 +23,19 @@ final class ImportRaceController extends AbstractController
     ) {
     }
 
-    public function __invoke(string $eventId, string $raceId): JsonResponse
-    {
+    public function __invoke(
+        #[CurrentUser]
+        UserAdapter $userAdapter,
+        string $eventId,
+        string $raceId,
+    ): JsonResponse {
+
         $nutritionPlanId = $this->idGenerator->generate();
         $this->commandBus->dispatch(new ImportRaceCommand(
             nutritionPlanId: $nutritionPlanId,
             externalEventId: $eventId,
             externalRaceId: $raceId,
-            runnerId: '2b09105c-9e61-482e-a44e-25de34441987' // @TODO USER
+            runnerId: $userAdapter->getUser()->id,
         ));
 
         return new JsonResponse(
