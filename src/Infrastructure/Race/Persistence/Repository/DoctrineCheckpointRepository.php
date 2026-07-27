5a035a8e6dd6148b56b1f11560afe75c61bd2aab
Lauric <lauric@mac.home>
2026-07-27 20:48:13 +0200

    Add Repo

--- src/Infrastructure/Race/Persistence/Repository/DoctrineCheckpointRepository.php
+++ src/Infrastructure/Race/Persistence/Repository/DoctrineCheckpointRepository.php
@@ -2,7 +2,21 @@
 
 namespace App\Infrastructure\Race\Persistence\Repository;
 
-final class DoctrineCheckpointRepository
-{
+use App\Domain\Race\Entity\Checkpoint;
+use App\Domain\Race\Repository\CheckpointRepositoryInterface;
+use App\Infrastructure\Shared\Persistence\Repository\DoctrineRepositoryTrait;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<Checkpoint>
+ */
+final class DoctrineCheckpointRepository extends ServiceEntityRepository implements CheckpointRepositoryInterface
+{
+    use DoctrineRepositoryTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Checkpoint::class);
+    }
 }
--- src/Infrastructure/Race/Persistence/Repository/DoctrineRunnerRaceRepository.php
+++ src/Infrastructure/Race/Persistence/Repository/DoctrineRunnerRaceRepository.php
@@ -2,7 +2,21 @@
 
 namespace App\Infrastructure\Race\Persistence\Repository;
 
-final class DoctrineRunnerRaceRepository
-{
+use App\Domain\Race\Entity\RunnerRace;
+use App\Domain\Race\Repository\RunnerRaceRepositoryInterface;
+use App\Infrastructure\Shared\Persistence\Repository\DoctrineRepositoryTrait;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @extends ServiceEntityRepository<RunnerRace>
+ */
+final class DoctrineRunnerRaceRepository extends ServiceEntityRepository implements RunnerRaceRepositoryInterface
+{
+    use DoctrineRepositoryTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, RunnerRace::class);
+    }
 }
