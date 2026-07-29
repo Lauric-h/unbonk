cbce995c476cd40e62957674b7adcc8df9161f80
Lauric-h <h.lauric@outlook.com>
2026-07-29 09:16:57 +0200

    Remove files

--- src/Application/NutritionPlan/UseCase/ListAllEvents/ListAllEventsQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Application\NutritionPlan\UseCase\ListAllEvents;
-
-use App\Domain\Shared\Bus\QueryInterface;
-
-final class ListAllEventsQuery implements QueryInterface
-{
-}
